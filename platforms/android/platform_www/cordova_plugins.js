3d1ae0035b77435f87caf38f0b631894f54838b7
rcespedes <rcportocarrero@gmail.com>
2015-12-21 00:26:32 -0500

    fx sqlite

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -5,12 +5,21 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.sqlite/www/SQLitePlugin.js",
+        "id": "com.phonegap.plugins.sqlite.SQLitePlugin",
+        "pluginId": "com.phonegap.plugins.sqlite",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.phonegap.plugins.sqlite": "0.7.0"
 }
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var myDB;
+var nombre_completo = '';
+var itemsToarray = new Array();
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,15 +37,36 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        //myDB = window.sqlitePlugin.openDatabase({name: "ardiles.sqlite"});
+        myDB = window.sqlitePlugin.importPrepopulatedDatabase({file: "ardiles.sqlite", "importIfExists": true});
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+        /*   var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    */
+     myDB.transaction(function(tx) {
+      tx.executeSql("select dni,nombres,ape_pat,ape_mat from tb_clientes;", [], function(tx, res) {
+        console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+        console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+        $('#movie-list').empty();
+        $.each(res.rows,function(index){
+            itemsToarray.push(res.rows.item(index));
+            var row = res.rows.item(index);
+            nombre_completo = row['nombres']+' '+row['ape_pat']+' '+row['ape_mat'];
+            $('#movie-list').append('<li data-filtertext="'+nombre_completo+'" ><a href="" data-id="' + row['id'] + '"><h3>' + nombre_completo + '</h3><p>Línea de crédito: 133</p></a></li>');
+        });
+        $('#movie-list').listview('refresh');
+      });
+
+    }, function(e) {
+      console.log("ERROR: " + e.message);
+    });
+  });
 
         console.log('Received Event: ' + id);
     }
